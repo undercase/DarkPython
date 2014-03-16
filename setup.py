fd530c2184b2fb79ec892fc527f6f722e3fa9ec1
Thomas Hobohm <superman3275@gmail.com>
2014-03-16 12:56:41 -0500

    Added short description / author to setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup, find_packages
 setup(
 	name="DarkPython",
 	version="1.0",
-	description="",
+	description="Pure-python IDE with custom debugger for use in the classroom.",
 	long_description="""
 	""",
-	author="",
+	author="Thomas Hobohm (superman3275)",
 	packages=find_packages(exclude='tests'),
 	package_data={'darkpython': ['assets/*.png']},
 	install_requires=['wxPython>=3.0'],
