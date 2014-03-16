db013c0466569b11552d8a44d9de83f90962db49
John Cipponeri <john.cipponeri@gmail.com>
2014-03-16 10:26:29 -0500

    Updated setup.py to be possibly functional
    Updated setup.py to be possibly functional

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
-print "Hello!"
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup(
+	name="DarkPython",
+	version="1.0",
+	description="",
+	long_description="""
+	""",
+	author="",
+	packages=find_packages(exclude='tests'),
+	#Not sure about this line: package_data={'darkpython': ['assets/*.png']},
+	install_requires=['wxPython>=3.0'],
+)
